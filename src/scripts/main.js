4a93570679abd021b0c7138080a3446485e2ae41
cnoss <christian.noss@fh-koeln.de>
2020-05-22 16:24:38 +0200

    Slideshow implementiert

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
+/**
+* toogleMenu
+*
+* Blendet das Menü aus oder ein.
+*
+*/
+
 function toogleMenu() {
   const interactionElementClass = '.js-navigation-interaction-element';
   const interactionElementAdditionalClass = 'hamburger-button--is-open';
@@ -12,4 +19,54 @@ function toogleMenu() {
     menuElement.classList.toggle(menuElementClass);
   });
 }
+
+
+/**
+* switchSlides
+*
+* Blättert in der Slideshow ein Bild vor oder zurück
+*
+*/
+function switchSlides() {
+  const slides = document.querySelectorAll('[data-js-slide]');
+  const interactionElementNext = document.querySelector('[data-js-nav-next-slide]');
+  const interactionElementPrevious = document.querySelector('[data-js-nav-previous-slide]');
+  const slideClassVisible = 'slide-show__slide--visible';
+  let activeSlide = 0;
+
+
+  function showSlide(activeSlide) {
+    slides[activeSlide].classList.add(slideClassVisible);
+  }
+
+  function hideSlide(activeSlide) {
+    slides[activeSlide].classList.remove(slideClassVisible);
+  }
+
+  function changeSlide(direction) {
+    hideSlide(activeSlide);
+
+    if (direction === 'next') {
+      if (activeSlide + 1 < slides.length) {
+        activeSlide += 1;
+      } else {
+        activeSlide = 0;
+      }
+    } else if (activeSlide - 1 < 0) {
+      activeSlide = slides.length - 1;
+    } else {
+      activeSlide -= 1;
+    }
+
+    showSlide(activeSlide);
+  }
+
+  showSlide(activeSlide);
+
+  interactionElementNext.addEventListener('click', () => { changeSlide('next'); });
+  interactionElementPrevious.addEventListener('click', () => { changeSlide('previous'); });
+}
+
+
 toogleMenu();
+switchSlides();
